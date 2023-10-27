9b8b79346f8fe2ce3b526f3c794200cf96b02990
SHANA SHABNAM NASIM <142229568+shananasim16@users.noreply.github.com>
2023-10-27 14:42:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses();
+        panel.classList.add('active');
+    });
+});
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+    });
+}
+
+// Initially, set the first panel as active
+panels[0].classList.add('active');
+
